bc6c22ddacd88f2b64249789f2e808fa1bde7f3e
Gasilin Maxim <63611476+MaximGasilin@users.noreply.github.com>
2024-02-06 23:52:25 +0300

    Исправление ошибки и корректировка названий
    Исправил ошибку. В прежней версии не хватает символа ';'.
    Подкорректировал названия полей и таблиц.

--- extensions/create_snowflecto_database_postgresql.sql
+++ extensions/create_snowflecto_database_postgresql.sql
@@ -7,9 +7,9 @@ GRANT TEMPORARY ON DATABASE snowflecto TO pgadminforouterconnection;
 \c snowflecto
 CREATE TABLE settings(name varchar(250) NOT NULL, value text, description text);
 INSERT INTO settings(name, value, description) VALUES ('limit_of_registries', '256', 'Количество реестров, которые можно будет создавать для генерации внутреннего кода. Минимальное значение - 3. Т.к. нулевой и последний реестры вспомогательные и нужны для предотвращения ситуаций с нехваткой номеров. В обычном режиме будут использватьсяя реестры со 1-го по 254. Нулевой хранит индекс самой первой капли. А 255-й - последней.'), 
-('limit_of_indexes_in_registry', '2147483648', 'Количество возможных индексов в каждом реестре.')
+('limit_of_indexes_in_registry', '2147483648', 'Количество возможных индексов в каждом реестре.');
 
-CREATE TABLE collections(id integer NOT NULL PRIMARY KEY, name varchar(250) NOT NULL, description text, composition_count integer NOT NULL);
+CREATE TABLE collections(id integer NOT NULL PRIMARY KEY, name varchar(250) NOT NULL, description text, composition_volume integer NOT NULL);
 
 CREATE TABLE water_vessels(collection integer NOT NULL, begin_drop_index numeric(1000) NOT NULL, end_drop_index numeric(1000) NOT NULL);
 
@@ -21,7 +21,7 @@ CREATE TABLE freezing_drops(collection integer NOT NULL, in_registry smallint NO
 
 CREATE TABLE freezers(id integer NOT NULL PRIMARY KEY, name varchar(250) not null, description text);
 
-CREATE TABLE composition(collection integer NOT NULL, drop_index numeric(1000) not null, quality numeric(15, 10) not null, freezer integer NOT NULL, parameters jsonpath);
+CREATE TABLE compositions(collection integer NOT NULL, drop_index numeric(1000) not null, quality numeric(15, 10) not null, freezer integer NOT NULL, parameters jsonpath);
 
 CREATE TABLE potential_snowflakes(collection integer not null, in_registry smallint NOT NULL, in_index integer NOT NULL, quality numeric(15, 10) not null);
 
